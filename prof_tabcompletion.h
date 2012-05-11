1eb0890fedc99f1875655912b2df4dee8a368b0e
James Booth <boothj5@gmail.com>
2012-05-11 01:42:52 +0100

    Added prof_tabcompletion

---
+++ prof_tabcompletion.h
@@ -0,0 +1,28 @@
+/* 
+ * prof_tabcompletion.h
+ *
+ * Copyright (C) 2012 James Booth <boothj5@gmail.com>
+ * 
+ * This file is part of Profanity.
+ *
+ * Profanity is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Profanity is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Profanity.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef PROF_TABCOMPLETION_H
+#define PROF_TABCOMPLETION_H
+
+typedef struct p_tabcompletion_t *PTabCompletion;
+
+#endif
