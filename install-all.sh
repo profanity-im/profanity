81a6d671d0ef6ef6c36e9a40e22c16f0cc87c5a5
James Booth <boothj5@gmail.com>
2012-07-10 02:14:07 +0100

    Added install-all.sh

---
+++ install-all.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+echo
+echo Profanity installer ... updating apt repositories
+echo
+sudo apt-get update
+
+echo
+echo Profanity installer... installing dependencies
+echo
+sudo apt-get -y install g++ autoconf libssl-dev libexpat1-dev libncurses5-dev libxml2-dev libglib2.0-dev libnotify-dev
+
+echo
+echo Profanity installer... installing head-unit
+echo
+git clone git://github.com/boothj5/head-unit.git
+cd head-unit
+make
+sudo make install
+
+cd ..
+
+echo
+echo Profanity installer... installing libstrophe
+echo
+git clone git://github.com/metajack/libstrophe.git
+cd libstrophe
+./bootstrap.sh
+./configure
+make
+sudo make install
+
+cd ..
+
+echo
+echo Profanity installer... installing Profanity
+echo
+./bootstrap.sh
+./configure
+make
+sudo make install
+
+echo
+echo Profanity installer... complete!
+echo
+
