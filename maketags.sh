57f0a407993bf46070e6bb844eb0fd88c648574f
James Booth <boothj5@gmail.com>
2012-10-22 21:57:01 +0100

    Removed scripts from root folder

--- maketags.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm -f tags
-rm -f cscope.out
-ctags -R .
-cscope -R -b
--- runvalgrind.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rm -f valgrind.out
-#valgrind --log-file=valgrind.out --leak-check=full --track-origins=yes --show-reachable=yes ./profanity
-valgrind --log-file=valgrind.out --leak-check=full --track-origins=yes ./profanity -l DEBUG
