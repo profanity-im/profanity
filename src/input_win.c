c10880ced492a81115349d66204daeeb26a75864
James Booth <boothj5@gmail.com>
2013-01-07 20:49:15 +0000

    Remove whitespace

--- src/input_win.c
+++ src/input_win.c
@@ -467,8 +467,6 @@ _printable(const wint_t ch)
     char bytes[MB_CUR_MAX+1];
     size_t utf_len = wcrtomb(bytes, ch, NULL);
     bytes[utf_len] = '\0';
- 
     gunichar unichar = g_utf8_get_char(bytes);
     return g_unichar_isprint(unichar) && (ch != KEY_MOUSE);
-
 }
