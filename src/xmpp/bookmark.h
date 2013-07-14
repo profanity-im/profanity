b9d29e9aa59a16bcfc06f7e91fd562688565e12a
Dmitry Podgorny <pasis.ua@gmail.com>
2013-07-14 23:58:02 +0300

    draft of bookmarks implementation
    '/bookmark add' and '/bookmark remove' ain't finished

--- src/xmpp/bookmark.h
+++ src/xmpp/bookmark.h
@@ -2,6 +2,21 @@
 #ifndef BOOKMARK_H
 #define BOOKMARK_H
 
+#include <glib.h>
+
+struct bookmark_t {
+    char *jid;
+    char *nick;
+    gboolean autojoin;
+};
+
+typedef struct bookmark_t Bookmark;
+
 void bookmark_request(void);
+void bookmark_add(const char *jid, const char *nick, gboolean autojoin);
+void bookmark_remove(const char *jid, gboolean autojoin);
+const GList *bookmark_get_list(void);
+char *bookmark_find(char *search_str);
+void bookmark_autocomplete_reset(void);
 
 #endif
