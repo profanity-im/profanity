d6c90ac7ca8bd3e32e353b36e1eab54ccabd5eb9
Dmitry Podgorny <pasis.ua@gmail.com>
2013-08-24 00:41:57 +0300

    fixed indenting

--- src/xmpp/presence.c
+++ src/xmpp/presence.c
@@ -493,7 +493,7 @@ _available_handler(xmpp_conn_t * const conn,
     }
 
     // self presence
-    if (strcmp(my_jid->barejid, from_jid->barejid) ==0) {
+    if (strcmp(my_jid->barejid, from_jid->barejid) == 0) {
         connection_add_available_resource(resource);
 
     // contact presence
