23842e5254f91ebecac37ceea6a28a240accae24
James Booth <boothj5@gmail.com>
2014-01-31 00:10:28 +0000

    Added muc room active tests

--- tests/test_muc.h
+++ tests/test_muc.h
@@ -5,3 +5,5 @@ void test_muc_add_invite(void **state);
 void test_muc_remove_invite(void **state);
 void test_muc_invite_count_0(void **state);
 void test_muc_invite_count_5(void **state);
+void test_muc_room_is_not_active(void **state);
+void test_muc_room_is_active(void **state);
