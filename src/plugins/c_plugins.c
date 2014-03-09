3acec2b0be23342e9200a3af375a35355eab30c7
James Booth <boothj5@gmail.com>
2014-03-09 01:27:35 +0000

    Udated copyright in plugins sources

--- src/plugins/c_plugins.c
+++ src/plugins/c_plugins.c
@@ -1,4 +1,25 @@
-// FIXME  on windows this might be a different header
+/*
+ * c_plugins.c
+ *
+ * Copyright (C) 2012 - 2014 James Booth <boothj5@gmail.com>
+ *
+ * This file is part of Profanity.
+ *
+ * Profanity is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Profanity is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Profanity.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <dlfcn.h>
 #include <stdlib.h>
 #include <assert.h>
