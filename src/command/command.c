cb614ec36f067b806b9896a2d37459e001982643
James Booth <boothj5@gmail.com>
2014-05-17 22:49:24 +0100

    Free autocomplete hash table

--- src/command/command.c
+++ src/command/command.c
@@ -1574,9 +1574,11 @@ _cmd_complete_parameters(char *input, int *size)
         if (result != NULL) {
             ui_replace_input(input, result, size);
             g_free(result);
+            g_hash_table_destroy(ac_funcs);
             return;
         }
     }
+    g_hash_table_destroy(ac_funcs);
 
     return;
 }
