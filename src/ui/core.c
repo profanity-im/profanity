30574e5334bd92ec4af4d6cff1c7ed9cacc7dde4
James Booth <boothj5@gmail.com>
2014-08-18 23:00:24 +0100

    Set window active instead of new on room subject

--- src/ui/core.c
+++ src/ui/core.c
@@ -1716,7 +1716,7 @@ _ui_room_subject(const char * const room_jid, const char * const subject)
 
     // not currenlty on groupchat window
     } else {
-        status_bar_new(num);
+        status_bar_active(num);
     }
 }
 
