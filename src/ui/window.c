a288c200d70e83138e404afa52968874255d9822
James Booth <boothj5@gmail.com>
2014-09-24 00:52:32 +0100

    Unreference date time

--- src/ui/window.c
+++ src/ui/window.c
@@ -311,6 +311,7 @@ win_show_status_string(ProfWin *window, const char * const from,
     if (last_activity != NULL) {
         GDateTime *now = g_date_time_new_now_local();
         GTimeSpan span = g_date_time_difference(now, last_activity);
+        g_date_time_unref(now);
 
         int hours = span / G_TIME_SPAN_HOUR;
         span = span - hours * G_TIME_SPAN_HOUR;
