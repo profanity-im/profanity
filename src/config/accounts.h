492c548aa69bf7c49793e8ffd29e063772bf356c
Peter Vilim <peterlvilim@users.noreply.github.com>
2015-01-06 23:00:02 -0600

    Add support for evaluated password

--- src/config/accounts.h
+++ src/config/accounts.h
@@ -61,6 +61,7 @@ void accounts_set_server(const char * const account_name, const char * const val
 void accounts_set_port(const char * const account_name, const int value);
 void accounts_set_resource(const char * const account_name, const char * const value);
 void accounts_set_password(const char * const account_name, const char * const value);
+void accounts_set_eval_password(const char * const account_name, const char * const value);
 void accounts_set_muc_service(const char * const account_name, const char * const value);
 void accounts_set_muc_nick(const char * const account_name, const char * const value);
 void accounts_set_otr_policy(const char * const account_name, const char * const value);
@@ -77,6 +78,7 @@ void accounts_set_priority_all(const char * const account_name, const gint value
 gint accounts_get_priority_for_presence_type(const char * const account_name,
     resource_presence_t presence_type);
 void accounts_clear_password(const char * const account_name);
+void accounts_clear_eval_password(const char * const account_name);
 void accounts_clear_server(const char * const account_name);
 void accounts_clear_port(const char * const account_name);
 void accounts_clear_otr(const char * const account_name);
