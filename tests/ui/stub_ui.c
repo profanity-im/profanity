22cc641905fcb6f321653a2826ef588d4cece223
Will Song <incertia9474@gmail.com>
2015-01-08 10:22:03 -0600

    add cons_exittitle_setting to tests

--- tests/ui/stub_ui.c
+++ tests/ui/stub_ui.c
@@ -433,6 +433,7 @@ void cons_time_setting(void) {}
 void cons_mouse_setting(void) {}
 void cons_statuses_setting(void) {}
 void cons_titlebar_setting(void) {}
+void cons_exittitle_setting(void) {}
 void cons_notify_setting(void) {}
 void cons_states_setting(void) {}
 void cons_outtype_setting(void) {}
