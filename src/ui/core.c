aec14848065765c0101f0ee09fbcf81c920a22d7
James Booth <boothj5@gmail.com>
2015-01-15 01:21:24 +0000

    Removed ch from main loop

--- src/ui/core.c
+++ src/ui/core.c
@@ -174,7 +174,7 @@ ui_close(void)
     endwin();
 }
 
-wint_t
+gboolean
 ui_get_char(char *input, int *size)
 {
     int result = 0;
@@ -193,7 +193,7 @@ ui_get_char(char *input, int *size)
         ui_input_nonblocking(FALSE);
     }
 
-    return ch;
+    return (ch != '\n');
 }
 
 void
