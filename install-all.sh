84de10e7cd940bd0290099f666ceebb5599e87f2
James Booth <boothj5@gmail.com>
2015-03-08 19:21:35 +0000

    Added readline-devel to instal-all.sh for fedora

--- install-all.sh
+++ install-all.sh
@@ -36,7 +36,7 @@ fedora_prepare()
 
     ARCH=`arch`
 
-    sudo yum -y install gcc git autoconf automake openssl-devel.$ARCH expat-devel.$ARCH ncurses-devel.$ARCH  glib2-devel.$ARCH libnotify-devel.$ARCH libcurl-devel.$ARCH libXScrnSaver-devel.$ARCH libotr3-devel.$ARCH libtool
+    sudo yum -y install gcc git autoconf automake openssl-devel.$ARCH expat-devel.$ARCH ncurses-devel.$ARCH  glib2-devel.$ARCH libnotify-devel.$ARCH libcurl-devel.$ARCH libXScrnSaver-devel.$ARCH libotr3-devel.$ARCH readline-devel.$ARCH libtool
 }
 
 opensuse_prepare()
