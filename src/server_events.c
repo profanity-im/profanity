385336c10b0026025d4cd3a32d0a9b1bc521469a
James Booth <boothj5@gmail.com>
2015-03-14 22:59:49 +0000

    Don't switch window on sent carbon

--- src/server_events.c
+++ src/server_events.c
@@ -310,7 +310,7 @@ handle_incoming_private_message(char *fulljid, char *message)
 
 void
 handle_carbon(char *barejid, char *message){
-    ui_outgoing_chat_msg("me", barejid, message);
+    ui_outgoing_chat_msg_carbon("me", barejid, message);
 }
 
 void
