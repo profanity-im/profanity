e896823ad838e59a3c1b2585e03b9dfa51288cfe
pszypowicz <pszypowicz@users.noreply.github.com>
2015-05-29 22:31:58 +0200

    Added libuuid-devel
    Package needed on fedora22

--- install-all.sh
+++ install-all.sh
@@ -37,6 +37,10 @@ fedora_prepare()
     ARCH=`arch`
 
     sudo yum -y install gcc git autoconf automake openssl-devel.$ARCH expat-devel.$ARCH ncurses-devel.$ARCH  glib2-devel.$ARCH libnotify-devel.$ARCH libcurl-devel.$ARCH libXScrnSaver-devel.$ARCH libotr3-devel.$ARCH readline-devel.$ARCH libtool uuid-devel.$ARCH
+    
+    if grep -q 22 /etc/fedora-release; then
+      dnf install libuuid-devel.$ARCH
+    fi
 }
 
 opensuse_prepare()
