2cb2f83ce3519a40f752ca2be7a07669a125ed1b
James Booth <boothj5@gmail.com>
2015-08-04 23:22:43 +0100

    Added test for missing resource on presence

--- tests/functionaltests/functionaltests.c
+++ tests/functionaltests/functionaltests.c
@@ -50,6 +50,7 @@ int main(int argc, char* argv[]) {
         PROF_FUNC_TEST(presence_set_priority),
         PROF_FUNC_TEST(presence_includes_priority),
         PROF_FUNC_TEST(presence_received),
+        PROF_FUNC_TEST(presence_missing_resource_defaults),
 
         PROF_FUNC_TEST(message_send),
         PROF_FUNC_TEST(message_receive),
