9593e17c3c414a4a78ec73bfee8464a78fa28dc1
James Booth <boothj5@gmail.com>
2015-09-27 23:53:06 +0100

    Include last activity in initial presence

--- src/event/client_events.h
+++ src/event/client_events.h
@@ -38,7 +38,7 @@
 jabber_conn_status_t cl_ev_connect_jid(const char * const jid, const char * const passwd, const char * const altdomain, const int port);
 jabber_conn_status_t cl_ev_connect_account(ProfAccount *account);
 
-void cl_ev_presence_send(const resource_presence_t presence_type, const char * const msg, const int idle);
+void cl_ev_presence_send(const resource_presence_t presence_type, const char * const msg, const int idle_secs);
 
 void cl_ev_send_msg(ProfChatWin *chatwin, const char * const msg);
 void cl_ev_send_muc_msg(ProfMucWin *mucwin, const char * const msg);
