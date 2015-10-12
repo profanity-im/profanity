118e92039ecbf2431838a51642582e1c72694e4f
James Booth <boothj5@gmail.com>
2015-10-13 00:32:02 +0100

    functionaltests: reset connect wait to 30 secs

--- tests/functionaltests/proftest.c
+++ tests/functionaltests/proftest.c
@@ -248,7 +248,7 @@ prof_connect_with_roster(char *roster)
     prof_input("password");
 
     // Allow time for profanity to connect
-    exp_timeout = 120;
+    exp_timeout = 30;
     assert_true(prof_output_regex("stabber@localhost logged in successfully, .+online.+ \\(priority 0\\)\\."));
     exp_timeout = 10;
     stbbr_wait_for("prof_presence_*");
