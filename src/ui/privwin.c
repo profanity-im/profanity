f9483371032bd962c686de782078ea46b0c1b42c
kiethandi <kieranthandi@gmail.com>
2015-11-20 00:34:03 +0000

    Update privwin.c

--- src/ui/privwin.c
+++ src/ui/privwin.c
@@ -34,13 +34,14 @@
 
 #include <assert.h>
 #include <glib.h>
+#include <stdlib.h>
 
 #include "ui/win_types.h"
 #include "ui/window.h"
 #include "ui/titlebar.h"
 #include "window_list.h"
 #include "config/preferences.h"
-#include "stdlib.h"
+
 
 void
 privwin_incoming_msg(ProfPrivateWin *privatewin, const char *const message, GDateTime *timestamp)
