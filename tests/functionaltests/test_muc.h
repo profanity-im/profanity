46a0e6ebed23560777e1387eeb4f2a6c8c311cc5
James Booth <boothj5@gmail.com>
2015-12-20 02:17:17 +0000

    Removed whitespace

--- tests/functionaltests/test_muc.h
+++ tests/functionaltests/test_muc.h
@@ -5,8 +5,3 @@ void sends_room_join_with_password(void **state);
 void sends_room_join_with_nick_and_password(void **state);
 void show_role_and_affiliation_on_join(void **state);
 void show_subject_on_join(void **state);
-
-
-
-
-
