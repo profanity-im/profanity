631c4f7d1b71bc5f22dfb369c1123024d25743c1
James Booth <boothj5@gmail.com>
2016-02-14 23:08:50 +0000

    Updated copyright

--- src/plugins/lua_api.c
+++ src/plugins/lua_api.c
@@ -1,7 +1,7 @@
 /*
  * lua_api.c
  *
- * Copyright (C) 2012 - 2014 James Booth <boothj5@gmail.com>
+ * Copyright (C) 2012 - 2016 James Booth <boothj5@gmail.com>
  *
  * This file is part of Profanity.
  *
