695694051e2f56d947ea8baf9199c1ff7bf81121
Paul Fariello <paul@fariello.eu>
2019-03-06 07:23:56 +0140

    Remove hints preventing carbons to work properly

--- src/xmpp/message.c
+++ src/xmpp/message.c
@@ -404,9 +404,6 @@ message_send_chat_omemo(const char *const jid, uint32_t sid, GList *keys,
         stanza_attach_state(ctx, message, state);
     }
 
-    stanza_attach_hints_no_copy(ctx, message);
-    stanza_attach_hints_no_store(ctx, message);
-
     if (request_receipt) {
         stanza_attach_receipt_request(ctx, message);
     }
