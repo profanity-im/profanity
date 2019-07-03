profanity-im/profanity

commit 13675fbf06a0d0fbae2b974596a0fb513ba22340

Only print room history for new messages upon reconnect
If re-establish a connection don't print the room history again.
In case there there happened nothing at all since we got the room
history on the last connection.
And in case there were no new messages during the time we have been
disconnected.

Instead of printing the room history again we now print 'Re-established
Connection'.

This adds a bit of overhead since we save the timestamp upon every MUC
message.

See: https://github.com/profanity-im/profanity/issues/704

src/event/server_events.c
@@ -66,6 +66,9 @@
 
 #include "ui/ui.h"
 
+gint _success_connections_counter = 0;
+GDateTime *_last_muc_message;
+
 void
 sv_ev_login_account_success(char *account_name, gboolean secured)
 {
@@ -101,6 +104,15 @@ sv_ev_login_account_success(char *account_name, gboolean secured)
     g_list_free(rooms);
 
     log_info("%s logged in successfully", account->jid);
+
+    // if we have been connected before
+    if (_success_connections_counter > 0)
+    {
+        cons_show("Connection re-established.");
+        wins_reestablished_connection();
+    }
+
+    _success_connections_counter++;
 
     if (account->startscript) {
         scripts_exec(account->startscript);
@@ -261,7 +273,19 @@ sv_ev_room_history(const char *const room_jid, const char *const nick,
     GDateTime *timestamp, const char *const message)
 {
     ProfMucWin *mucwin = wins_get_muc(room_jid);
-    if (mucwin) {
+
+    // if this is the first successful connection
+    if (_success_connections_counter == 1) {
+        // save timestamp of last received muc message
+        // so we dont display, if there was no activity in channel, once we reconnect
+        if (_last_muc_message) {
+            g_date_time_unref(_last_muc_message);
+        }
+        _last_muc_message  = g_date_time_new_now_local();
+    }
+
+    gboolean younger = g_date_time_compare(_last_muc_message, timestamp) < 0 ? TRUE : FALSE;
+    if (mucwin && (_success_connections_counter == 1 || younger )) {
         mucwin_history(mucwin, nick, timestamp, message);
     }
 }
@@ -337,6 +361,12 @@ sv_ev_room_message(const char *const room_jid, const char *const nick, const cha
             mucwin->unread_triggers = TRUE;
         }
     }
+
+    // save timestamp of last received muc message
+    if (_last_muc_message) {
+        g_date_time_unref(_last_muc_message);
+    }
+    _last_muc_message  = g_date_time_new_now_local();
 
     if (prefs_do_room_notify(is_current, mucwin->roomjid, mynick, nick, new_message, mention, triggers != NULL)) {
         Jid *jidp = jid_create(mucwin->roomjid);

src/ui/window_list.c
@@ -846,6 +846,27 @@ wins_lost_connection(void)
 }
 
 void
+wins_reestablished_connection(void)
+{
+    GList *values = g_hash_table_get_values(windows);
+    GList *curr = values;
+
+    while (curr) {
+        ProfWin *window = curr->data;
+        if (window->type != WIN_CONSOLE) {
+            win_println(window, THEME_TEXT, '-', "Connection re-established.");
+
+            // if current win, set current_win_dirty
+            if (wins_is_current(window)) {
+                win_update_virtual(window);
+            }
+        }
+        curr = g_list_next(curr);
+    }
+    g_list_free(values);
+}
+
+void
 wins_swap(int source_win, int target_win)
 {
     ProfWin *source = g_hash_table_lookup(windows, GINT_TO_POINTER(source_win));

src/ui/window_list.h
@@ -83,6 +83,7 @@ void wins_resize_all(void);
 GSList* wins_get_chat_recipients(void);
 GSList* wins_get_prune_wins(void);
 void wins_lost_connection(void);
+void wins_reestablished_connection(void);
 gboolean wins_tidy(void);
 GSList* wins_create_summary(gboolean unread);
 void wins_destroy(void);

commit ad9263520e8c3c151d48ba579bece5f705c2131e

Show MUC history on intentional reconnect
Small bug caused by 13675fb and ce5a4ed.
Fix https://github.com/profanity-im/profanity/issues/1142

src/event/client_events.c
@@ -89,6 +89,8 @@ cl_ev_disconnect(void)
 
     ui_close_all_wins();
     ev_disconnect_cleanup();
+    // on intentional disconnect reset the counter
+    ev_reset_connection_counter();
 }
 
 void

src/event/common.c
@@ -47,6 +47,8 @@
 #include "omemo/omemo.h"
 #endif
 
+static gint _success_connections_counter = 0;
+
 void
 ev_disconnect_cleanup(void)
 {
@@ -66,3 +68,32 @@ ev_disconnect_cleanup(void)
 #endif
 }
 
+gboolean
+ev_was_connected_already(void)
+{
+    if (_success_connections_counter > 0)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+gboolean
+ev_is_first_connect(void)
+{
+    if (_success_connections_counter == 1)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+void
+ev_inc_connection_counter(void)
+{
+    _success_connections_counter++;
+}
+
+void
+ev_reset_connection_counter(void)
+{
+    _success_connections_counter = 0;
+}

src/event/common.h
@@ -36,5 +36,9 @@
 #define EVENT_COMMON_H
 
 void ev_disconnect_cleanup(void);
+void ev_inc_connection_counter(void);
+void ev_reset_connection_counter(void);
+gboolean ev_was_connected_already(void);
+gboolean ev_is_first_connect(void);
 
 #endif

src/event/server_events.c
@@ -67,8 +67,6 @@
 
 #include "ui/ui.h"
 
-gint _success_connections_counter = 0;
-
 void
 sv_ev_login_account_success(char *account_name, gboolean secured)
 {
@@ -104,13 +102,13 @@ sv_ev_login_account_success(char *account_name, gboolean secured)
     log_info("%s logged in successfully", account->jid);
 
     // if we have been connected before
-    if (_success_connections_counter > 0)
+    if (ev_was_connected_already())
     {
         cons_show("Connection re-established.");
         wins_reestablished_connection();
     }
 
-    _success_connections_counter++;
+    ev_inc_connection_counter();
 
     if (account->startscript) {
         scripts_exec(account->startscript);
@@ -261,7 +259,7 @@ sv_ev_room_subject(const char *const room, const char *const nick, const char *c
 {
     muc_set_subject(room, subject);
     ProfMucWin *mucwin = wins_get_muc(room);
-    if (mucwin && muc_roster_complete(room) && _success_connections_counter == 1) {
+    if (mucwin && muc_roster_complete(room) && ev_is_first_connect()) {
         mucwin_subject(mucwin, nick, subject);
     }
 }
@@ -272,7 +270,7 @@ sv_ev_room_history(ProfMessage *message)
     ProfMucWin *mucwin = wins_get_muc(message->jid->barejid);
     if (mucwin) {
         // if this is the first successful connection
-        if (_success_connections_counter == 1) {
+        if (ev_is_first_connect()) {
             // save timestamp of last received muc message
             // so we dont display, if there was no activity in channel, once we reconnect
             if (mucwin->last_msg_timestamp) {
@@ -282,7 +280,7 @@ sv_ev_room_history(ProfMessage *message)
         }
 
         gboolean younger = g_date_time_compare(mucwin->last_msg_timestamp, message->timestamp) < 0 ? TRUE : FALSE;
-        if (_success_connections_counter == 1 || younger ) {
+        if (ev_is_first_connect() || younger ) {
             mucwin_history(mucwin, message->jid->resourcepart, message->timestamp, message->plain);
         }
     }