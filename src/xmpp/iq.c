c07d533ec1b7fe471a58586f577edc43867971a8
Michael Vetter <jubalh@iodoru.org>
2019-07-12 11:11:45 +0200

    Fixup for last commit

--- src/xmpp/iq.c
+++ src/xmpp/iq.c
@@ -771,7 +771,6 @@ void
 iq_cancel_command_config(ProfConfWin *confwin)
 {
     xmpp_ctx_t * const ctx = connection_get_ctx();
-    CommandConfigData *data = (CommandConfigData *)confwin->userdata;
     xmpp_stanza_t *iq = stanza_create_room_config_cancel_iq(ctx, confwin->roomjid);
     iq_send_stanza(iq);
     xmpp_stanza_release(iq);
