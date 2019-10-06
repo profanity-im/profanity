1d23d6461dfe920e96af11a5e6395756c2b7d2c6
Michael Vetter <jubalh@iodoru.org>
2019-10-06 18:37:55 +0200

    Remove not needed strdupin test_cmd_rooms
    Regards https://github.com/profanity-im/profanity/issues/1019

--- tests/unittests/test_cmd_rooms.c
+++ tests/unittests/test_cmd_rooms.c
@@ -46,7 +46,7 @@ void cmd_rooms_uses_account_default_when_no_arg(void **state)
     gchar *args[] = { NULL };
 
     ProfAccount *account = account_new("testaccount", NULL, NULL, NULL, TRUE, NULL, 0, NULL, NULL, NULL,
-        0, 0, 0, 0, 0, strdup("default_conf_server"), NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+        0, 0, 0, 0, 0, "default_conf_server", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
     will_return(connection_get_status, JABBER_CONNECTED);
     will_return(session_get_account_name, "account_name");
@@ -83,8 +83,9 @@ void cmd_rooms_filter_arg_used_when_passed(void **state)
 {
     gchar *args[] = { "filter", "text", NULL };
 
+
     ProfAccount *account = account_new("testaccount", NULL, NULL, NULL, TRUE, NULL, 0, NULL, NULL, NULL,
-        0, 0, 0, 0, 0, strdup("default_conf_server"), NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+        0, 0, 0, 0, 0, "default_conf_server", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
     will_return(connection_get_status, JABBER_CONNECTED);
     will_return(session_get_account_name, "account_name");
