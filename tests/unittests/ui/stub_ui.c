83b61e5160b6624eb0b2898c1321d33341e6510c
Michael Vetter <jubalh@iodoru.org>
2020-02-10 14:59:10 +0100

    xep-0308: add cons_correction_setting stub to tests

--- tests/unittests/ui/stub_ui.c
+++ tests/unittests/ui/stub_ui.c
@@ -452,6 +452,7 @@ void cons_winpos_setting(void) {}
 void cons_statusbar_setting(void) {}
 void cons_tray_setting(void) {}
 void cons_os_setting(void) {}
+void cons_correction_setting(void) {}
 
 void cons_show_contact_online(PContact contact, Resource *resource, GDateTime *last_activity)
 {
