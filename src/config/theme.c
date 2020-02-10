3c1db1cd3297c00547e3dfee7df50010f07660bc
Michael Vetter <jubalh@iodoru.org>
2020-02-10 14:58:22 +0100

    xep-0308: make correction.char themeable

--- src/config/theme.c
+++ src/config/theme.c
@@ -483,6 +483,14 @@ _load_preferences(void)
         }
     }
 
+    if (g_key_file_has_key(theme, "ui", "correction.char", NULL)) {
+        gchar *ch = g_key_file_get_string(theme, "ui", "correction.char", NULL);
+        if (ch && strlen(ch) > 0) {
+            prefs_set_omemo_char(ch[0]);
+            g_free(ch);
+        }
+    }
+
     // load window positions
     if (g_key_file_has_key(theme, "ui", "titlebar.position", NULL) &&
             g_key_file_has_key(theme, "ui", "mainwin.position", NULL) &&
