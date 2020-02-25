profanity-im/profanity

commit e9c5c1979d836ed75c37d48651710b4fd125cfb2

Fix memleak in _handle_groupchat()
We need to unref the timestamp before setting a new one.

src/xmpp/message.c
@@ -872,7 +872,12 @@ _handle_groupchat(xmpp_stanza_t *const stanza)
         message->timestamp = stanza_get_delay_from(stanza, jid->domainpart);
     }
 
-    bool is_muc_history = message->timestamp != NULL;
+    bool is_muc_history;
+    if (message->timestamp != NULL) {
+        is_muc_history = TRUE;
+        g_date_time_unref(message->timestamp);
+        message->timestamp = NULL;
+    }
 
     // we want to display the oldest delay
     message->timestamp = stanza_get_oldest_delay(stanza);

commit 779c2efc1710ed5d8b4fa60e00e85731b7bb7bd7

Set muc history correctly in _handle_groupchat()
Fix init. mistake introduced in e9c5c1979d836ed75c37d48651710b4fd125cfb2

src/xmpp/message.c
@@ -872,7 +872,7 @@ _handle_groupchat(xmpp_stanza_t *const stanza)
         message->timestamp = stanza_get_delay_from(stanza, jid->domainpart);
     }
 
-    bool is_muc_history;
+    bool is_muc_history = FALSE;
     if (message->timestamp != NULL) {
         is_muc_history = TRUE;
         g_date_time_unref(message->timestamp);