7419d2945125f8ade9d91e8895187d59df6fd311
Michael Vetter <jubalh@iodoru.org>
2020-04-07 11:31:17 +0200

    Update database stub

--- tests/unittests/database/stub_database.c
+++ tests/unittests/database/stub_database.c
@@ -27,9 +27,7 @@
 #include "database.h"
 
 gboolean log_database_init(ProfAccount *account) { return TRUE; }
-void log_database_add_incoming_chat(ProfMessage *message) {}
-void log_database_add_incoming_muc(ProfMessage *message) {}
-void log_database_add_incoming_muc_pm(ProfMessage *message) {}
+void log_database_add_incoming(ProfMessage *message) {}
 void log_database_add_outgoing_chat(const char * const id, const char * const barejid, const char * const message, const char *const replace_id, prof_enc_t enc) {}
 void log_database_add_outgoing_muc(const char * const id, const char * const barejid, const char * const message, const char *const replace_id, prof_enc_t enc) {}
 void log_database_add_outgoing_muc_pm(const char * const id, const char * const barejid, const char * const message, const char *const replace_id, prof_enc_t enc) {}
