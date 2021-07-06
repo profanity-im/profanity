46fe69de9b4049c5c4bf6fddb3dfba4480b13caa
Paul Fariello <paul@fariello.eu>
2021-07-06 17:47:33 +0200

    Ensure devicelist access model is configured before updating it

--- src/xmpp/omemo.h
+++ src/xmpp/omemo.h
@@ -38,6 +38,7 @@
 #include "xmpp/iq.h"
 
 void omemo_devicelist_subscribe(void);
+void omemo_devicelist_configure_and_request(void);
 void omemo_devicelist_publish(GList* device_list);
 void omemo_devicelist_request(const char* const jid);
 void omemo_bundle_publish(gboolean first);
