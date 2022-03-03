1afe2aad24224d165493c69ebd1d0523fa777cdc
MarcoPolo-PasTonMolo <marcopolopastonmolo@protonmail.com>
2022-03-03 09:59:21 +0200

    Fix not autocompleting nicks with '/'
    Remove check for '/' at the beginning of the line before autocompleting
    and make it fallback to command autocompletion if no nicks found.
    
    Fixes https://github.com/profanity-im/profanity/issues/1474

--- src/ui/inputwin.c
+++ src/ui/inputwin.c
@@ -583,14 +583,16 @@ _inp_rl_tab_handler(int count, int key)
     }
 
     ProfWin* current = wins_get_current();
-    if ((strncmp(rl_line_buffer, "/", 1) != 0) && (current->type == WIN_MUC)) {
+    if (current->type == WIN_MUC) {
         char* result = muc_autocomplete(current, rl_line_buffer, FALSE);
         if (result) {
             rl_replace_line(result, 1);
             rl_point = rl_end;
             free(result);
         }
-    } else if (strncmp(rl_line_buffer, "/", 1) == 0) {
+    }
+
+    if (strncmp(rl_line_buffer, "/", 1) == 0) {
         ProfWin* window = wins_get_current();
         char* result = cmd_ac_complete(window, rl_line_buffer, FALSE);
         if (result) {
@@ -611,14 +613,16 @@ _inp_rl_shift_tab_handler(int count, int key)
     }
 
     ProfWin* current = wins_get_current();
-    if ((strncmp(rl_line_buffer, "/", 1) != 0) && (current->type == WIN_MUC)) {
+    if (current->type == WIN_MUC) {
         char* result = muc_autocomplete(current, rl_line_buffer, TRUE);
         if (result) {
             rl_replace_line(result, 1);
             rl_point = rl_end;
             free(result);
         }
-    } else if (strncmp(rl_line_buffer, "/", 1) == 0) {
+    }
+
+    if (strncmp(rl_line_buffer, "/", 1) == 0) {
         ProfWin* window = wins_get_current();
         char* result = cmd_ac_complete(window, rl_line_buffer, TRUE);
         if (result) {
