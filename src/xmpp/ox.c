f891edc0e994e6b05deaf40b964a6d384f9fe32e
Michael Vetter <jubalh@iodoru.org>
2022-04-14 09:23:12 +0200

    Fix typo Annonuce -> Announce

--- src/xmpp/ox.c
+++ src/xmpp/ox.c
@@ -91,8 +91,8 @@ ox_announce_public_key(const char* const filename)
 {
     assert(filename);
 
-    cons_show("Annonuce OpenPGP Key for OX %s ...", filename);
-    log_info("[OX] Annonuce OpenPGP Key of OX: %s", filename);
+    cons_show("Announce OpenPGP Key for OX %s ...", filename);
+    log_info("[OX] Announce OpenPGP Key of OX: %s", filename);
 
     // key the key and the fingerprint via GnuPG from file
     char* key = NULL;
@@ -104,7 +104,7 @@ ox_announce_public_key(const char* const filename)
         return FALSE;
     }
 
-    log_info("[OX] Annonuce OpenPGP Key for Fingerprint: %s", fp);
+    log_info("[OX] Announce OpenPGP Key for Fingerprint: %s", fp);
     xmpp_ctx_t* const ctx = connection_get_ctx();
     char* id = xmpp_uuid_gen(ctx);
     xmpp_stanza_t* iq = xmpp_iq_new(ctx, STANZA_TYPE_SET, id);
@@ -231,7 +231,7 @@ ox_request_public_key(const char* const jid, const char* const fingerprint)
 void
 _ox_metadata_node__public_key(const char* const fingerprint)
 {
-    log_info("Annonuce OpenPGP metadata: %s", fingerprint);
+    log_info("Announce OpenPGP metadata: %s", fingerprint);
     assert(fingerprint);
     assert(strlen(fingerprint) == KEYID_LENGTH);
     // iq
