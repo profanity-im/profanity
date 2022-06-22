c0e27f5e7296d4d293c7392724e6c4d0e25a4da9
Michael Vetter <jubalh@iodoru.org>
2022-06-22 13:10:42 +0200

    Fix formatting

--- src/ui/console.c
+++ src/ui/console.c
@@ -2020,7 +2020,7 @@ cons_log_setting(void)
         cons_show("Shared log (/log shared)    : OFF");
 
     log_level_t filter = log_get_filter();
-    const gchar *level = log_string_from_level(filter);
+    const gchar* level = log_string_from_level(filter);
     cons_show("Log level (/log level)      : %s", level);
 }
 
