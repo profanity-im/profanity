dba87d1d27c8af409381fc203a3e044983c4a1cd
Sean Lewis <sean@atsec.com>
2025-06-16 14:08:06 -0500

    Modified README with some cleanup, and edited run.sh to reflect changes in README

--- docker/run-scripts/README.md
+++ docker/run-scripts/README.md
@@ -14,10 +14,29 @@ $HOME == full path to your home directory (Ex: /home/alice)
 
 $CMD run -v $HOME/.config/profanity:/root/.config/profanity -it <NAME>:latest
 
-The "run.sh" implements a wrapper around just such a call. Edit the script for your
-environment. 
+The "run.sh" script implements a wrapper around just such a call. To run: 
 
-## Theme support issues (Remote systems, and Docker containers)
+1. Edit the script for your environment. Look for section labeled "User editable section".
+2. Edit the variables with "##" in the lead, within this section. 
+3. Stop editing when you see a line beginning with " ### END...".
+4. Run the following chmod command on the shell script: 
+   <user@computer> chmod +x run.sh
+5. Run the script to start profanity
+   ./run.sh
+6. Profanity should pop-up automatically in the tab/terminal as expected.
+
+## Notes
+
+Some high level notes on limitations 
+
+### Notifications
+
+External OS notifications will not work. While this was not tested, it's almost 100% 
+certain this is the case, running profanity from a Docker container. You may not care
+about this lack of functionality, but it will be a problem for those who do care such
+as the author who relies on this functionality (as well as "psychic mode"). 
+
+### Theme support issues (Remote systems, and Docker containers)
 
 You'll see on remote systems sometimes that certain themes simply will not load (As in, SSH
 into remote, then run profanity from there). 
--- docker/run-scripts/run.sh
+++ docker/run-scripts/run.sh
@@ -2,50 +2,58 @@
 ######################
 # A quick and dirty shell script to run profanity with a user mounted config file.
 #
+# *NIX
+#
 # This is for *nix based systems of course. However Profanity deals with pathing
 # may need to be recognized as a multi-option scenario here. 
 #
-# Example Windows Addition (which I have no idea how to work with)
-#   # WINDOWS USERS
-#   # 
-#   # Comment out "user_home" and the other "config_dir". Use this instead.
-#   # Naturally where this lives largely depends on how profanity was installed 
-#   # or works on Windows. 
+# Example Windows Addition (which the author has no idea how to work with)
+#  # WINDOWS USERS
+#  # 
+#  # Comment out "user_home" and the other "config_dir". Use this instead.
+#  # Naturally where this lives largely depends on how profanity was installed 
+#  # or works on Windows. 
 #   config_dir='C:\%APPDATA%\profanity'
 # 
 ######################
 
 ######################
-  # User Editable section
+  ### User Editable section
+	# Instructions
+	#   Modify variables with "##" above them. Leave the rest
+	#   alone. 
+	#
+	## Edit user_home, to the appropriate user path.
   user_home='/home/example'
+	# Leave this alone. 
   config_dir="$user_home/.config/profanity"
-  # Set the toolname you use to execute profanity in a container.
+  ## Set the toolname you use to execute profanity images.
   virt_tool='docker'
-  # The name of the container to boot up (should you utilize multiple). 
+  ## The name of the container to boot up (should you utilize multiple). 
   img_name='profanity.debian'
+	### END User Editable section
 ######################
 
 
 ######################
-# The meat and potatoes 
+# Meat and potatoes 
 #
 # Notes
-#   In my testing themes are a problem in Docker containers. I believe this
-#   is just a limitation of how Docker bridges data in and out over terminal
-#   but I didn't test it thoroughly.
+#   In my testing themes are a problem in Docker containers. This appears to be
+#   a limitation of terminal in these spaces.
 #
 #   Example 
+#     Copied everforest theme to: 
+#       $user_home/.config/profanity/themes/everforest
 #
-#   Copied everforest theme to: 
-#     $user_home/.config/profanity/themes/everforest
-#
-#     It did not work despite the config being set correctly. 
+#       It did not work despite the config being set correctly. 
 #
 ######################
+#
 # shutdown previous iteration first, and assume --rm is present in primary run.
 $virt_tool stop $img_name
 $virt_tool run -it --rm \
   -v $config_dir:/root/.config/profanity \
-  $img_name profanity
+  $img_name
 
 
