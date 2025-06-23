profanity-im/profanity

commit 75d766387677c783694df9ccfda9dffbb3bf4ef7

Free wins summary list

src/ui/console.c
@@ -251,6 +251,7 @@ cons_show_wins(void)
         win_save_println(console, curr->data);
         curr = g_slist_next(curr);
     }
+    g_slist_free_full(window_strings, free);
 
     cons_show("");
     cons_alert();

commit dd8da16c96e4cb8d3afd38216cee69b8056375fe

Use correct `free` function.
Fixes: 75d76638 ("Free wins summary list")

Signed-off-by: Steffen Jaeckel <s@jaeckel.eu>

src/ui/console.c
@@ -484,7 +484,7 @@ cons_show_wins(gboolean unread)
         }
         curr = g_slist_next(curr);
     }
-    g_slist_free_full(window_strings, free);
+    g_slist_free_full(window_strings, g_free);
 
     cons_alert(NULL);
 }
@@ -505,7 +505,7 @@ cons_show_wins_attention()
         }
         curr = g_slist_next(curr);
     }
-    g_slist_free_full(window_strings, free);
+    g_slist_free_full(window_strings, g_free);
 
     cons_alert(NULL);
 }