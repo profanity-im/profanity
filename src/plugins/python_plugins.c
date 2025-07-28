profanity-im/profanity

commit c0da36c48d9878c7e90a796d97f5670fd686d7aa

Rage-cleanup.
While trying to get the unit tests working again I stumbled over all those
things that I thought could be better^TM.

Now we also know "TODO: why does this make the test fail?" - because
the unit tests are brittle AF ... and we have to init the subsystems
we use in the test, otherwise the cleanup will fail...

BTW. you can now also only run a single test ... or a pattern or so ...
you'd have to read how `cmocka_set_test_filter()` works exactly.

Signed-off-by: Steffen Jaeckel <s@jaeckel.eu>

src/plugins/python_plugins.c
@@ -69,12 +69,6 @@ disable_python_threads()
     PyEval_RestoreThread(thread_state);
 }
 
-static void
-_unref_module(PyObject* module)
-{
-    Py_XDECREF(module);
-}
-
 const char*
 python_get_version_string(void)
 {
@@ -95,14 +89,15 @@ python_get_version_number(void)
 void
 python_env_init(void)
 {
-    loaded_modules = g_hash_table_new_full(g_str_hash, g_str_equal, free, (GDestroyNotify)_unref_module);
+    loaded_modules = g_hash_table_new_full(g_str_hash, g_str_equal, free, (GDestroyNotify)Py_XDECREF);
 
     python_init_prof();
 
     auto_gchar gchar* plugins_dir = files_get_data_path(DIR_PLUGINS);
     auto_gchar gchar* path = g_strdup_printf(
-          "import sys\n"
-          "sys.path.append(\"%s/\")\n", plugins_dir);
+        "import sys\n"
+        "sys.path.append(\"%s/\")\n",
+        plugins_dir);
 
     PyRun_SimpleString(path);
     python_check_error();

Makefile.am
@@ -357,6 +357,9 @@ check-unit: tests/unittests/unittests
 format: $(all_c_sources)
 	clang-format -i $(all_c_sources)
 
+format-sources: $(core_sources) $(main_source)
+	clang-format -i $^
+
 spell:
 	codespell
 

src/command/cmd_ac.c
@@ -1648,6 +1648,7 @@ cmd_ac_uninit(void)
     autocomplete_free(plugins_reload_ac);
     autocomplete_free(script_show_ac);
     g_hash_table_destroy(ac_funcs);
+    ac_funcs = NULL;
 }
 
 static void

src/command/cmd_defs.c
@@ -2845,7 +2845,9 @@ _cmd_uninit(void)
 {
     cmd_ac_uninit();
     g_hash_table_destroy(commands);
+    commands = NULL;
     g_hash_table_destroy(search_index);
+    search_index = NULL;
 }
 
 /*

src/log.c
@@ -54,6 +54,8 @@
 
 #define PROF "prof"
 
+static void _log_msg(log_level_t level, const char* const area, const char* const msg);
+
 static FILE* logp;
 static gchar* mainlogfile = NULL;
 static gboolean user_provided_log = FALSE;
@@ -121,14 +123,22 @@ _log_abbreviation_string_from_level(log_level_t level)
     }
 }
 
+static gboolean
+_should_log(log_level_t level)
+{
+    return level >= level_filter && logp;
+}
+
 void
 log_debug(const char* const msg, ...)
 {
+    if (!_should_log(PROF_LEVEL_DEBUG))
+        return;
     va_list arg;
     va_start(arg, msg);
     GString* fmt_msg = g_string_new(NULL);
     g_string_vprintf(fmt_msg, msg, arg);
-    log_msg(PROF_LEVEL_DEBUG, PROF, fmt_msg->str);
+    _log_msg(PROF_LEVEL_DEBUG, PROF, fmt_msg->str);
     g_string_free(fmt_msg, TRUE);
     va_end(arg);
 }
@@ -136,11 +146,13 @@ log_debug(const char* const msg, ...)
 void
 log_info(const char* const msg, ...)
 {
+    if (!_should_log(PROF_LEVEL_INFO))
+        return;
     va_list arg;
     va_start(arg, msg);
     GString* fmt_msg = g_string_new(NULL);
     g_string_vprintf(fmt_msg, msg, arg);
-    log_msg(PROF_LEVEL_INFO, PROF, fmt_msg->str);
+    _log_msg(PROF_LEVEL_INFO, PROF, fmt_msg->str);
     g_string_free(fmt_msg, TRUE);
     va_end(arg);
 }
@@ -148,11 +160,13 @@ log_info(const char* const msg, ...)
 void
 log_warning(const char* const msg, ...)
 {
+    if (!_should_log(PROF_LEVEL_WARN))
+        return;
     va_list arg;
     va_start(arg, msg);
     GString* fmt_msg = g_string_new(NULL);
     g_string_vprintf(fmt_msg, msg, arg);
-    log_msg(PROF_LEVEL_WARN, PROF, fmt_msg->str);
+    _log_msg(PROF_LEVEL_WARN, PROF, fmt_msg->str);
     g_string_free(fmt_msg, TRUE);
     va_end(arg);
 }
@@ -160,11 +174,13 @@ log_warning(const char* const msg, ...)
 void
 log_error(const char* const msg, ...)
 {
+    if (!_should_log(PROF_LEVEL_ERROR))
+        return;
     va_list arg;
     va_start(arg, msg);
     GString* fmt_msg = g_string_new(NULL);
     g_string_vprintf(fmt_msg, msg, arg);
-    log_msg(PROF_LEVEL_ERROR, PROF, fmt_msg->str);
+    _log_msg(PROF_LEVEL_ERROR, PROF, fmt_msg->str);
     g_string_free(fmt_msg, TRUE);
     va_end(arg);
 }
@@ -206,28 +222,34 @@ log_close(void)
     }
 }
 
+static void
+_log_msg(log_level_t level, const char* const area, const char* const msg)
+{
+    GDateTime* dt = g_date_time_new_now_local();
+
+    char* level_str = _log_abbreviation_string_from_level(level);
+
+    auto_gchar gchar* date_fmt = g_date_time_format_iso8601(dt);
+
+    fprintf(logp, "%s: %s: %s: %s\n", date_fmt, area, level_str, msg);
+    g_date_time_unref(dt);
+
+    fflush(logp);
+
+    if (prefs_get_boolean(PREF_LOG_ROTATE) && !user_provided_log) {
+        long result = ftell(logp);
+        if (result != -1 && result >= prefs_get_max_log_size()) {
+            _rotate_log_file();
+        }
+    }
+}
+
 void
 log_msg(log_level_t level, const char* const area, const char* const msg)
 {
-    if (level >= level_filter && logp) {
-        GDateTime* dt = g_date_time_new_now_local();
-
-        char* level_str = _log_abbreviation_string_from_level(level);
-
-        auto_gchar gchar* date_fmt = g_date_time_format_iso8601(dt);
-
-        fprintf(logp, "%s: %s: %s: %s\n", date_fmt, area, level_str, msg);
-        g_date_time_unref(dt);
-
-        fflush(logp);
-
-        if (prefs_get_boolean(PREF_LOG_ROTATE) && !user_provided_log) {
-            long result = ftell(logp);
-            if (result != -1 && result >= prefs_get_max_log_size()) {
-                _rotate_log_file();
-            }
-        }
-    }
+    if (!_should_log(level))
+        return;
+    _log_msg(level, area, msg);
 }
 
 int

src/plugins/api.c
@@ -111,7 +111,7 @@ api_register_command(const char* const plugin_name, const char* command_name, in
                      char** synopsis, const char* description, char* arguments[][2], char** examples,
                      void* callback, void (*callback_exec)(PluginCommand* command, gchar** args), void (*callback_destroy)(void* callback))
 {
-    PluginCommand* command = malloc(sizeof(PluginCommand));
+    PluginCommand* command = calloc(1, sizeof(PluginCommand));
     command->command_name = strdup(command_name);
     command->min_args = min_args;
     command->max_args = max_args;
@@ -119,28 +119,22 @@ api_register_command(const char* const plugin_name, const char* command_name, in
     command->callback_exec = callback_exec;
     command->callback_destroy = callback_destroy;
 
-    CommandHelp* help = malloc(sizeof(CommandHelp));
-
-    help->tags[0] = NULL;
+    CommandHelp* help = calloc(1, sizeof(CommandHelp));
 
     int i;
     for (i = 0; synopsis[i] != NULL; i++) {
         help->synopsis[i] = strdup(synopsis[i]);
     }
-    help->synopsis[i] = NULL;
 
     help->desc = strdup(description);
     for (i = 0; arguments[i][0] != NULL; i++) {
         help->args[i][0] = strdup(arguments[i][0]);
         help->args[i][1] = strdup(arguments[i][1]);
     }
-    help->args[i][0] = NULL;
-    help->args[i][1] = NULL;
 
     for (i = 0; examples[i] != NULL; i++) {
         help->examples[i] = strdup(examples[i]);
     }
-    help->examples[i] = NULL;
 
     command->help = help;
 

src/plugins/callbacks.c
@@ -104,7 +104,8 @@ _free_command(PluginCommand* command)
     }
     free(command->command_name);
 
-    _free_command_help(command->help);
+    if (command->help)
+        _free_command_help(command->help);
 
     free(command);
 }
@@ -118,6 +119,8 @@ _free_command_hash(GHashTable* command_hash)
 static void
 _free_timed_function(PluginTimedFunction* timed_function)
 {
+    if (!timed_function)
+        return;
     if (timed_function->callback_destroy) {
         timed_function->callback_destroy(timed_function->callback);
     }
@@ -178,8 +181,11 @@ void
 callbacks_close(void)
 {
     g_hash_table_destroy(p_window_callbacks);
+    p_window_callbacks = NULL;
     g_hash_table_destroy(p_timed_functions);
+    p_timed_functions = NULL;
     g_hash_table_destroy(p_commands);
+    p_commands = NULL;
 }
 
 void

src/profanity.c
@@ -298,7 +298,10 @@ _call_and_free_shutdown_routine(struct shutdown_routine* r)
 void
 prof_shutdown(void)
 {
-    g_list_free_full(shutdown_routines, (GDestroyNotify)_call_and_free_shutdown_routine);
+    if (shutdown_routines) {
+        g_list_free_full(shutdown_routines, (GDestroyNotify)_call_and_free_shutdown_routine);
+        shutdown_routines = NULL;
+    }
 }
 
 static void

commit 9fc960dd609b17feb02e36dd0e9be53906f26693

Trampoline Python unref again.
In the past `Py_XDECREF()` was a macro. Preserve compat to ancient Python
versions by having a trampoline which calls `Py_XDECREF()`.

Fixes: #2043
Fixes: c0da36c4 ("Rage-cleanup.")

Signed-off-by: Steffen Jaeckel <s@jaeckel.eu>

src/plugins/python_plugins.c
@@ -86,10 +86,16 @@ python_get_version_number(void)
     return version_number;
 }
 
+static void
+_unref_module(PyObject* module)
+{
+    Py_XDECREF(module);
+}
+
 void
 python_env_init(void)
 {
-    loaded_modules = g_hash_table_new_full(g_str_hash, g_str_equal, free, (GDestroyNotify)Py_XDECREF);
+    loaded_modules = g_hash_table_new_full(g_str_hash, g_str_equal, free, (GDestroyNotify)_unref_module);
 
     python_init_prof();
 