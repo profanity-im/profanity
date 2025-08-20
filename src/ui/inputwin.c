profanity-im/profanity

commit 3c56b289ed04f8525affd532d0192d653e1dcd95

Add slashguard feature
New command `/slashguard` tries to protect against typing ` /quit` by
not allowing a slash in the first 4 characters.

src/ui/inputwin.c
@@ -196,6 +196,13 @@ inp_readline(void)
     }
 
     if (inp_line) {
+        if (!get_password && prefs_get_boolean(PREF_SLASH_GUARD)) {
+            char *res = (char*) memchr (inp_line+1, '/', 3);
+            if (res) {
+                cons_show("Your text contains a slash in the first 4 characters");
+                return NULL;
+            }
+        }
         return strdup(inp_line);
     } else {
         return NULL;

src/command/cmd_ac.c
@@ -1567,7 +1567,7 @@ _cmd_ac_complete_params(ProfWin *window, const char *const input, gboolean previ
 
     // autocomplete boolean settings
     gchar *boolean_choices[] = { "/beep", "/intype", "/states", "/outtype", "/flash", "/splash",
-        "/history", "/vercheck", "/privileges", "/wrap", "/carbons", "/lastactivity", "/os"};
+        "/history", "/vercheck", "/privileges", "/wrap", "/carbons", "/lastactivity", "/os", "/slashguard"};
 
     for (i = 0; i < ARRAY_SIZE(boolean_choices); i++) {
         result = autocomplete_param_with_func(input, boolean_choices[i], prefs_autocomplete_boolean_choice, previous, NULL);

src/command/cmd_defs.c
@@ -2418,6 +2418,23 @@ static struct cmd_t command_defs[] =
             { "message",    "The corrected message."})
         CMD_NOEXAMPLES
     },
+
+    { "/slashguard",
+        parse_args, 1, 1, &cons_slashguard_setting,
+        CMD_NOSUBFUNCS
+        CMD_MAINFUNC(cmd_slashguard)
+        CMD_TAGS(
+            CMD_TAG_UI,
+            CMD_TAG_CHAT)
+        CMD_SYN(
+            "/slashguard on|off")
+        CMD_DESC(
+            "Slashguard won't accept a slash in the first 4 characters of your input field. "
+            "It tries to protect you from typing ' /quit' and similar things in chats.")
+        CMD_ARGS(
+            { "on|off", "Enable or disable slashguard." })
+        CMD_NOEXAMPLES
+    },
 };
 
 static GHashTable *search_index;

src/command/cmd_funcs.c
@@ -8836,3 +8836,15 @@ cmd_correct(ProfWin *window, const char *const command, gchar **args)
     win_println(window, THEME_DEFAULT, "!", "Command /correct only valid in regular chat windows.");
     return TRUE;
 }
+
+gboolean
+cmd_slashguard(ProfWin *window, const char *const command, gchar **args)
+{
+    if (args[0] == NULL) {
+        return FALSE;
+    }
+
+    _cmd_set_boolean_preference(args[0], command, "Slashguard", PREF_SLASH_GUARD);
+
+    return TRUE;
+}

src/command/cmd_funcs.h
@@ -231,4 +231,5 @@ gboolean cmd_avatar(ProfWin *window, const char *const command, gchar **args);
 gboolean cmd_os(ProfWin *window, const char *const command, gchar **args);
 gboolean cmd_correction(ProfWin *window, const char *const command, gchar **args);
 gboolean cmd_correct(ProfWin *window, const char *const command, gchar **args);
+gboolean cmd_slashguard(ProfWin *window, const char *const command, gchar **args);
 #endif

src/config/preferences.c
@@ -1744,6 +1744,7 @@ _get_group(preference_t pref)
         case PREF_STATUSBAR_ROOM:
         case PREF_TITLEBAR_MUC_TITLE:
         case PREF_HISTORY_COLOR_MUC:
+        case PREF_SLASH_GUARD:
             return PREF_GROUP_UI;
         case PREF_STATES:
         case PREF_OUTTYPE:
@@ -2051,6 +2052,8 @@ _get_key(preference_t pref)
             return "history.muc.color";
         case PREF_AVATAR_CMD:
             return "avatar.cmd";
+        case PREF_SLASH_GUARD:
+            return "slashguard";
         default:
             return NULL;
     }

src/config/preferences.h
@@ -167,6 +167,7 @@ typedef enum {
     PREF_CORRECTION_ALLOW,
     PREF_HISTORY_COLOR_MUC,
     PREF_AVATAR_CMD,
+    PREF_SLASH_GUARD,
 } preference_t;
 
 typedef struct prof_alias_t {

src/ui/console.c
@@ -2050,6 +2050,16 @@ cons_avatar_setting(void)
 }
 
 void
+cons_slashguard_setting(void)
+{
+    if (prefs_get_boolean(PREF_SLASH_GUARD)) {
+        cons_show("Slashguard (/slashguard)       : ON");
+    } else {
+        cons_show("Slashguard (/slashguard)       : OFF");
+    }
+}
+
+void
 cons_show_connection_prefs(void)
 {
     cons_show("Connection preferences:");

commit 7f48452d84c052927b25975052e578d325a147a6

Don't use `memchr()` on strings potentially shorter than 4 bytes.
When running profanity under Valgrind with slashguard enabled, the
following error could occur:

```
[...]
==4021347== Invalid read of size 1
==4021347==    at 0x4851F49: memchr (vg_replace_strmem.c:986)
==4021347==    by 0x45CEAD: _inp_slashguard_check (inputwin.c:183)
==4021347==    by 0x45CEAD: inp_readline (inputwin.c:225)
==4021347==    by 0x431184: prof_run (profanity.c:121)
==4021347==    by 0x42C609: main (main.c:176)
==4021347==  Address 0xe850883 is 0 bytes after a block of size 3 alloc'd
==4021347==    at 0x48477C4: malloc (vg_replace_malloc.c:446)
[...]
```

`memchr()` requires the complete memory that shall be searched to be
accessible. Using `strchr()` could work for shorter strings, but we only
want to search in the first 4 chars.

Instead of somehow working around those limitations, simply search manually
in the first 4 bytes.

Fixes: 3c56b289 ("Add slashguard feature")
Signed-off-by: Steffen Jaeckel <s@jaeckel.eu>

src/ui/inputwin.c
@@ -180,11 +180,15 @@ _inp_slashguard_check(void)
         return false;
     if (!prefs_get_boolean(PREF_SLASH_GUARD))
         return false;
-    if (memchr(inp_line + 1, '/', 3)) {
-        cons_show("Your text contains a slash in the first 4 characters");
-        free(inp_line);
-        inp_line = NULL;
-        return true;
+    size_t n = 1;
+    while (inp_line[n] != '\0' && n < 4) {
+        if (inp_line[n] == '/') {
+            cons_show("Your text contains a slash in the first 4 characters");
+            free(inp_line);
+            inp_line = NULL;
+            return true;
+        }
+        n++;
     }
     return false;
 }