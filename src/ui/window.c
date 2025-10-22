profanity-im/profanity

commit f27fa98717631abaa647d4224235d4fafb00a7f9

Some minor changes
* Fix some linter suggestions.
* Change some defines to `const` vars.
* Free buffer entries in the reverse order they were allocated.

Signed-off-by: Steffen Jaeckel <s@jaeckel.eu>

src/ui/window.c
@@ -61,13 +61,11 @@
 #include "ui/screen.h"
 #include "xmpp/xmpp.h"
 #include "xmpp/roster_list.h"
-#include "xmpp/connection.h"
-#include "database.h"
 
-#define PAD_SIZE        1000
-#define LOADING_MESSAGE "Loading older messages…"
-#define CONS_WIN_TITLE "Profanity. Type /help for help information."
-#define XML_WIN_TITLE  "XML Console"
+static const int PAD_SIZE = 100;
+static const char* LOADING_MESSAGE = "Loading older messages…";
+static const char* CONS_WIN_TITLE = "Profanity. Type /help for help information.";
+static const char* XML_WIN_TITLE = "XML Console";
 
 #define CEILING(X) (X - (int)(X) > 0 ? (int)(X + 1) : (int)(X))
 

src/ui/buffer.c
@@ -213,12 +213,12 @@ _create_entry(const char* show_char, int pad_indent, GDateTime* time, int flags,
 static void
 _free_entry(ProfBuffEntry* entry)
 {
-    free(entry->show_char);
-    free(entry->message);
-    free(entry->display_from);
-    free(entry->from_jid);
     free(entry->id);
-    free(entry->receipt);
+    free(entry->message);
+    free(entry->from_jid);
+    free(entry->display_from);
     g_date_time_unref(entry->time);
+    free(entry->show_char);
+    free(entry->receipt);
     free(entry);
 }

commit 584631a645fd5950e794a5ce84603f032e06f8e7

Set PAD_SIZE back to 1000
PAD_SIZE was set to 100 in f27fa98
Set PAD_SIZE back to 1000 to fix the `Ncurses Overflow! ... pos: 99, ...` problem.

Fixes: #2045
Fixes: f27fa98

src/ui/window.c
@@ -62,7 +62,7 @@
 #include "xmpp/xmpp.h"
 #include "xmpp/roster_list.h"
 
-static const int PAD_SIZE = 100;
+static const int PAD_SIZE = 1000;
 static const char* LOADING_MESSAGE = "Loading older messages…";
 static const char* CONS_WIN_TITLE = "Profanity. Type /help for help information.";
 static const char* XML_WIN_TITLE = "XML Console";