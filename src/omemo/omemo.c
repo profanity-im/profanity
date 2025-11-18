profanity-im/profanity

commit 5b6b5130ae7b5bc901d9e43a8eacd24a04a21059

Fix OMEMO keyfile loading
We have to lazy load the keyfiles as they will maybe be written at one
point and should be initialized by that time.

Fixup of ca2df180d81f1b30142e63cd7621a00eb9248913

Signed-off-by: Steffen Jaeckel <jaeckel-floss@eyet-services.de>

src/omemo/omemo.c
@@ -230,24 +230,22 @@ omemo_on_connect(ProfAccount* account)
         return;
     }
 
-    if (!load_custom_keyfile(&omemo_ctx.identity, g_strdup_printf("%s/%s", omemo_dir, "identity.txt")) || !_load_identity()) {
-        return;
+    if (load_custom_keyfile(&omemo_ctx.identity, g_strdup_printf("%s/%s", omemo_dir, "identity.txt"))) {
+        if (!_load_identity())
+            return;
     }
 
-    if (!load_custom_keyfile(&omemo_ctx.trust, g_strdup_printf("%s/%s", omemo_dir, "trust.txt"))) {
-        return;
+    if (load_custom_keyfile(&omemo_ctx.trust, g_strdup_printf("%s/%s", omemo_dir, "trust.txt"))) {
+        _load_trust();
     }
-    _load_trust();
 
-    if (!load_custom_keyfile(&omemo_ctx.sessions, g_strdup_printf("%s/%s", omemo_dir, "sessions.txt"))) {
-        return;
+    if (load_custom_keyfile(&omemo_ctx.sessions, g_strdup_printf("%s/%s", omemo_dir, "sessions.txt"))) {
+        _load_sessions();
     }
-    _load_sessions();
 
-    if (!load_custom_keyfile(&omemo_ctx.knowndevices, g_strdup_printf("%s/%s", omemo_dir, "known_devices.txt"))) {
-        return;
+    if (load_custom_keyfile(&omemo_ctx.knowndevices, g_strdup_printf("%s/%s", omemo_dir, "known_devices.txt"))) {
+        _load_known_devices();
     }
-    _load_known_devices();
 
     omemo_devicelist_subscribe();
 }

commit 79ff9bad7d02c61ed687fca0d7b9360274a69bab

Fix OMEMO startup
When setting up OMEMO for the first time via `/omemo gen` one had
to reconnect in order to make OMEMO work. This is fixed now.

Fixes: 5b6b5130 ("Fix OMEMO keyfile loading")
Signed-off-by: Steffen Jaeckel <s@jaeckel.eu>

src/omemo/omemo.c
@@ -145,6 +145,32 @@ omemo_init(void)
     omemo_static_data.fingerprint_ac = g_hash_table_new_full(g_str_hash, g_str_equal, free, (GDestroyNotify)autocomplete_free);
 }
 
+static gboolean
+_omemo_finalize_identity_load(ProfAccount* account)
+{
+    auto_gchar gchar* omemo_dir = files_file_in_account_data_path(DIR_OMEMO, account->jid, NULL);
+    if (!omemo_dir) {
+        log_error("[OMEMO] failed creating directory");
+        return FALSE;
+    }
+
+    if (load_custom_keyfile(&omemo_ctx.trust, g_strdup_printf("%s/%s", omemo_dir, "trust.txt"))) {
+        _load_trust();
+    }
+
+    if (load_custom_keyfile(&omemo_ctx.sessions, g_strdup_printf("%s/%s", omemo_dir, "sessions.txt"))) {
+        _load_sessions();
+    }
+
+    if (load_custom_keyfile(&omemo_ctx.knowndevices, g_strdup_printf("%s/%s", omemo_dir, "known_devices.txt"))) {
+        _load_known_devices();
+    }
+
+    omemo_devicelist_subscribe();
+
+    return TRUE;
+}
+
 void
 omemo_on_connect(ProfAccount* account)
 {
@@ -243,19 +269,7 @@ omemo_on_connect(ProfAccount* account)
             return;
     }
 
-    if (load_custom_keyfile(&omemo_ctx.trust, g_strdup_printf("%s/%s", omemo_dir, "trust.txt"))) {
-        _load_trust();
-    }
-
-    if (load_custom_keyfile(&omemo_ctx.sessions, g_strdup_printf("%s/%s", omemo_dir, "sessions.txt"))) {
-        _load_sessions();
-    }
-
-    if (load_custom_keyfile(&omemo_ctx.knowndevices, g_strdup_printf("%s/%s", omemo_dir, "known_devices.txt"))) {
-        _load_known_devices();
-    }
-
-    omemo_devicelist_subscribe();
+    _omemo_finalize_identity_load(account);
 }
 
 void
@@ -323,7 +337,8 @@ omemo_generate_crypto_materials(ProfAccount* account)
 
     omemo_identity_keyfile_save();
 
-    omemo_ctx.loaded = TRUE;
+    if ((omemo_ctx.loaded = _omemo_finalize_identity_load(account)) == FALSE)
+        return;
 
     omemo_publish_crypto_materials();
     omemo_start_sessions();
@@ -340,7 +355,7 @@ omemo_publish_crypto_materials(void)
         return;
     }
 
-    omemo_bundle_publish(true);
+    omemo_bundle_publish(TRUE);
 }
 
 static void